f74d7c499b01a5166373e2ce413d99afeadeddee
Oleksandr Zhabenko <33298924+OleksandrZhabenko@users.noreply.github.com>
2020-04-15 21:27:22 +0300

    Update PhonesAndBells.hs

--- PhonesAndBells.hs
+++ PhonesAndBells.hs
@@ -44,3 +44,6 @@ h1 f (x, y) j = do
     print herr
     partialTest_k v0 0 ts
     mixTest
+
+-- README.markdown is available online at: https://hackage.haskell.org/package/dobutokO2-0.28.0.0/src/README.markdown
+main = testSoundGen2G "README.markdown" (\t -> V.fromList [(0.05763181818181818 * t, 0.3598),(1.112159090909091 * t, 0.4588962),(2 * t, 0.6853),(3 * t, 0.268),(4 * t, 0.6823),(5 * t, 0.53)]) 0.4 "Ой у лузі червона калина"
