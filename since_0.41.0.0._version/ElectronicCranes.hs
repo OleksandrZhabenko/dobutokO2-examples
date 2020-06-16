b296a72ed6488b39ab25c64a6229dc7d6ec9ecc8
Oleksandr Zhabenko <33298924+OleksandrZhabenko@users.noreply.github.com>
2020-06-16 09:04:58 +0300

    Update ElectronicCranes.hs

--- since_0.41.0.0._version/ElectronicCranes.hs
+++ since_0.41.0.0._version/ElectronicCranes.hs
@@ -10,5 +10,5 @@ import DobutokO.Sound.Functional.Params
 import Data.Vector (fromList)
 
 main :: IO ()
-main = uniqOverSoXSynthNGen4E6GSPar "../result00345.wav" (P2s 38 15 "Ukrainian Dorian scale") 0.1 0.4 0.01 "Доброго ранку!" 
+main = uniqOverSoXSynthNGen4E6GSPar "ABC.wav" (P2s 38 15 "Ukrainian Dorian scale") 0.1 0.4 0.01 "Доброго ранку!" 
   "Добрий день!" "Добрий вечір!" (fromList ([2,3,2,3,2,3,8,9,8,9,8,9,4,0,0,12,0,0]::[Int])) "А завтра буде вівторок." "А сьогодні понеділок" 0.001
